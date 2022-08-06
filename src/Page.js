be437f8b127674a8dd29c9ed2d857028e52edad4
Edward Kabue <ekabue0@gmail.com>
2022-08-06 12:35:16 +0300

    Finished version

--- src/Page.js
+++ src/Page.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import PostContent from './components/PostContent';
+import BlogList from './components/BlogList';
+import BlogPost from './components/BlogPost';
+
 const Page = ({ page }) => {
   if (page) {
     switch (page?.meta?.type) {
       case 'bloglist':
-        return <div>Blog List</div>;
+        return <BlogList blogs={page.data} />;
       case 'blogpost':
-        return <div>Blog Post</div>;
+        return <BlogPost blog={page} />;
       case 'page':
         return <PostContent content={page.content || []} />;
       default:
